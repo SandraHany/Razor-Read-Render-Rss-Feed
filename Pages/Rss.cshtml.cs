c34973886a6fae0cd58acec2335c044bec3d5623
sandrahanyh <67375539+SandraHany@users.noreply.github.com>
2023-07-03 17:09:42 +0300

    refactored

--- Pages/Rss.cshtml.cs
+++ Pages/Rss.cshtml.cs
@@ -1,34 +1,49 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Xml;
 
 namespace WebApplication1.Pages
 {
     public class RssModel : PageModel
     {
-        public List<RssModelClass> RssList { get; set; }
+        public List<RssModelClass> RssList { get; set; } = new();
 
         public async Task<IActionResult> OnGetAsync()
         {
             RssList = new List<RssModelClass>();
-            var doc = new XmlDocument();
-            using (var reader = new XmlTextReader("http://scripting.com/rss.xml"))
+
+            using (HttpClient client = new HttpClient())
             {
-                doc.Load(reader);
+                var response = await client.GetAsync("http://scripting.com/rss.xml");
+                if (response.IsSuccessStatusCode)
+                {
+                    var xmlContent = await response.Content.ReadAsStringAsync();
+                    var doc = new XmlDocument();
+                    doc.LoadXml(xmlContent);
+
+                    foreach (XmlNode node in doc.SelectNodes("/rss/channel/item"))
+                    {
+                        var modelObject = new RssModelClass();
+                        modelObject.Guid = node["guid"].InnerText;
+                        modelObject.Description = node["description"].InnerText;
+                        modelObject.PubDate = node["pubDate"].InnerText;
+                        modelObject.Link = node["link"].InnerText;
+                        RssList.Add(modelObject);
+                    }
+                }
+                else
+                {
+                    return StatusCode((int)response.StatusCode);
+                }
             }
-            var ModelList = new RssModelClass();
-            foreach (XmlNode node in doc.SelectNodes("/rss/channel/item"))
-            {
-                var ModelObject = new RssModelClass();
-                ModelObject.Guid = node["guid"].InnerText;
-                ModelObject.Description = node["description"].InnerText;
-                ModelObject.PubDate = node["pubDate"].InnerText;
-                ModelObject.Link = node["link"].InnerText;
-                RssList.Add(ModelObject);
-            }
+
             return Page();
         }
     }
+
     public class RssModelClass
     {
         public string Description { get; set; }
@@ -36,6 +51,5 @@ namespace WebApplication1.Pages
         public string Link { get; set; }
         public string Guid { get; set; }
         public string Text { get; set; }
-        
     }
 }
